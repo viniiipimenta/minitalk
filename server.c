9582b38a7ea4aa914c95c24a4b1a3588ede07c18
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-08-08 19:45:26 -0300

    Add files via upload

--- server.c
+++ server.c
@@ -4,19 +4,19 @@
 void handle_signal(int sig)
 {
 	static int byte;
-	static int bit_i;
+	static int bit;
 
-	if (!bit_i || bit_i > 8)
+	if (!bit || bit > 8)
 	{
-		bit_i = 1;
+		bit = 1;
 		byte = 0;
 	}
 	byte = byte << 1;
 	if (sig == SIGUSR1)
 		byte++;
-	if (bit_i == 8)
+	if (bit == 8)
 		write(1, &byte, 1);
-	bit_i++;
+	bit++;
 }
 
 int main(void)
