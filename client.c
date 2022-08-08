a1abacca4e4678bc6dc0076a47f10ebe785cb081
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-08-08 19:34:07 -0300

    Add files via upload

--- client.c
+++ client.c
@@ -1,35 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/08 13:58:36 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/08/08 14:53:18 by mpimenta         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "./ft_printf/ft_printf.h"
 #include <signal.h>
 
-void send_signal(char *bits, int pid)
+static void send_signal(char *bits, int pid)
 {
 	int i;
 
 	i = 0;
-	while (bits[i])
+	while (bits[i] != '\0')
 	{
 		if (bits[i] == '1')
 			kill(pid, SIGUSR1);
-		else
+		else if (bits[i] == '0')
 			kill(pid, SIGUSR2);
 		i++;
-		usleep(200);
+		usleep(800);
 	}
 }
 
-void convert(char str, int pid)
+static void convert(char str, int pid)
 {
 	char bits[9];
 	size_t j;
@@ -49,7 +37,7 @@ int main(int argc, char *argv[])
 	int i;
 	int pid;
 
-	if (argc != 3)
+	if (argc < 3 || argc > 4)
 		return (0);
 	i = 0;
 	pid = ft_atoi(argv[1]);
--- server.c
+++ server.c
@@ -1,54 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/08 13:58:21 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/08/08 14:56:57 by mpimenta         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "./ft_printf/ft_printf.h"
 #include <signal.h>
-#include <stdlib.h>
 
 void handle_signal(int sig)
 {
-	static int			c;
-	static int	i;
+	static int byte;
+	static int bit_i;
 
-	c = 0;
-	i = 0;
-	if (sig == SIGUSR2)
+	if (!bit_i || bit_i > 8)
 	{
-		c = c << 1;
-		i++;
+		bit_i = 1;
+		byte = 0;
 	}
-	else
-	{
-		c = c << 1;
-		c = c | 1;
-		i++;
-	}
-	if (i == 8)
-	{
-		write(1, &c, 1);
-		i = 0;
-	}
-
+	byte = byte << 1;
+	if (sig == SIGUSR1)
+		byte++;
+	if (bit_i == 8)
+		write(1, &byte, 1);
+	bit_i++;
 }
 
 int main(void)
 {
-    int		pid;
+    int     pid;
 
-	pid = getpid();
-	ft_printf("%d\n", pid);
+    pid = getpid();
+    ft_printf("PID --> %d\n", pid);
 	signal(SIGUSR1, handle_signal);
 	signal(SIGUSR2, handle_signal);
 	while (1)
 		pause();
-	return (0);
+    return (0);
 }
